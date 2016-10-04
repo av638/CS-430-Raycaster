85963d3963eb1f3f950ffde4312794b6ac02f5ae
av638 <av638@nau.edu>
2016-10-03 20:33:45 -0700

    Fixed my problem with a warning that came from every I created an executable in gcc so I removed that from the read me. Also updated the vector/dot math header file

--- README.md
+++ README.md
@@ -2,11 +2,9 @@
 In this second project we are tasked with creating a simple raycaster.
 
 The main has the actual ray casting and the intersection functions. Everything else helps with the parsing
-and the writing. The only thing that gives you a slight warning is that I am returning a local variable when
-doing the subtraction of vectors. I do this because I was having trouble saving a pointer to the vector that 
-I got from the subtraction function. Otherwise it still works just fine.
+and the writing.
 
 USE: Command Line Args
-width height outfile name(remember to use quotes!)
+width height outfileName jsonFile
 
-ex: 200 200 "hi.ppm"
+ex: 200 200 hi.ppm objects.json
--- dotMath.h
+++ dotMath.h
@@ -4,31 +4,31 @@ typedef double* v3;
 
 //inline will tell c to only compile when it is actually being used in the program
 //static tells the compiler to put this into a single object and not share it with others
-static inline double* v3_add(v3 x, v3 y, v3 z)
+static inline void v3_add(v3 x, v3 y, v3 c)
 {
-    v3 c;
+    //v3 c;
     c[0] = x[0] + y[0];
     c[1] = x[1] + y[1];
     c[2] = x[2] + y[2];
-    return c;
+    //return c;
 
 }
 
-static inline double*  v3_subtract(v3 x, v3 y)
+static inline void  v3_subtract(v3 x, v3 y, v3 c)
 {
-    double c[3];
+    //double c[3];
     c[0] = x[0] - y[0];
     c[1] = x[1] - y[1];
     c[2] = x[2] - y[2];
-    return c;
+    //return c;
 }
-static inline double* v3_scale(v3 x, double s, v3 z)
+static inline void v3_scale(v3 x, double s, v3 z)
 {    v3 c;
 
     c[0] = x[0] * z[0];
     c[1] = x[1] * z[1];
     c[2] = x[2] * z[2];
-    return c;
+//    return c;
 
 }
 
@@ -39,13 +39,13 @@ static inline double v3_dot(v3 a, v3 b)
 
 }
 
-static inline double* v3_cross(v3 a, v3 b, v3 c)
+static inline void v3_cross(v3 a, v3 b, v3 c)
 {
 
     c[0] = a[1]*b[2] - a[2]*b[1];
     c[1] = a[2]*b[0] - a[0]*b[2];
     c[2] = a[0]*b[1] - a[1]*b[0];
-    return c;
+ //   return c;
 }
 
 
