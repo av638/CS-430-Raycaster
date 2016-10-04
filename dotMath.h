3d28f0c94b517df08c101296954808e64b4c456b
av638 <av638@nau.edu>
2016-10-03 17:39:21 -0700

    Changed the header cause it didn't like the return types when compiled.

--- dotMath.h
+++ dotMath.h
@@ -4,7 +4,7 @@ typedef double* v3;
 
 //inline will tell c to only compile when it is actually being used in the program
 //static tells the compiler to put this into a single object and not share it with others
-static inline void v3_add(v3 x, v3 y, v3 z)
+static inline double* v3_add(v3 x, v3 y, v3 z)
 {
     v3 c;
     c[0] = x[0] + y[0];
@@ -22,7 +22,7 @@ static inline double*  v3_subtract(v3 x, v3 y)
     c[2] = x[2] - y[2];
     return c;
 }
-static inline void v3_scale(v3 x, double s, v3 z)
+static inline double* v3_scale(v3 x, double s, v3 z)
 {    v3 c;
 
     c[0] = x[0] * z[0];
@@ -39,14 +39,14 @@ static inline double v3_dot(v3 a, v3 b)
 
 }
 
-/*static inline double v3_cross(v3 a, v3 b, v3 c)
+static inline double* v3_cross(v3 a, v3 b, v3 c)
 {
-    v3 c;
+
     c[0] = a[1]*b[2] - a[2]*b[1];
     c[1] = a[2]*b[0] - a[0]*b[2];
     c[2] = a[0]*b[1] - a[1]*b[0];
     return c;
-}*/
+}
 
 
 #endif // DOTMATH_H_INCLUDED
